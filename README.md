7d4bce3d1435b0e4216c96e7c2d7605aae7980e3
firman <dev.aikosia@gmail.com>
2023-09-27 16:49:15 +0700

    mulai develop

---
+++ README.md
@@ -0,0 +1,17 @@
+# Migratiom
+
+wajib Backup database sebelum migrasi.
+
+1. Settings > Inventory > Products 
+   Disable-kan:
+   - Variants
+   - Units Of Measure
+   - Product Packagings
+
+   Settings > Purchase > Products
+   Disable-kan:
+   - Variants
+   - Variant Grid Entry
+
+2. Archive kan semua produk di menu Purchase > Products > Product
+
---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Camis Product',
+    'version': '0.0.1',
+    'author': 'Camis Development Team',
+    'summary': 'Modifikasi purchase > products, inventory > products, sales > products',
+    'sequence': 1,
+    'description': 'Menyesuaikan struktur data products sesuai dengan kebutuhan bisnis CA',
+    'category': 'Camis',
+    'website': 'https://www.aikosia.com',
+    'depends': [
+        'camis_base'
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
